5c01f67f5aaa22a9a1ead9b45444873ad60b582c
michimani <michimani210@gmail.com>
2022-09-30 08:29:35 +0900

    add: dev function

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -59,3 +59,18 @@ resource "aws_iam_policy_attachment" "policy_attachment_for_function_role" {
   policy_arn = aws_iam_policy.policy_for_function.arn
 }
 
+
+
+resource "aws_lambda_function" "container_lambda_function_dev" {
+  function_name = "container-lambda-function-dev"
+  role          = aws_iam_role.role_for_lambda.arn
+  package_type  = "Image"
+  image_uri     = "${aws_ecr_repository.container_lambda_repository.repository_url}:dev"
+  timeout       = 10
+  memory_size   = 128
+  environment {
+    variables = {
+      "ENV" = "dev"
+    }
+  }
+}
