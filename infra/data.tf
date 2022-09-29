47d2930d0ece2f27dd1b9b4e4a7f78b5f794acac
michimani <michimani210@gmail.com>
2022-09-30 01:12:52 +0900

    fix: OIDC thumbprint list

--- infra/data.tf
+++ infra/data.tf
@@ -3,3 +3,7 @@ data "aws_caller_identity" "current" {}
 output "account_id" {
   value = data.aws_caller_identity.current.account_id
 }
+
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+}
--- infra/iam.tf
+++ infra/iam.tf
@@ -2,7 +2,7 @@
 resource "aws_iam_openid_connect_provider" "github_actiohns_provider" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = []
+  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
 }
 
 resource "aws_iam_role" "iam_role_for_github_actions" {
